e8078a33ef4b47019c048627e60efebffbd253f9
chaeunho <97829504+chaeunho1227@users.noreply.github.com>
2025-04-07 19:28:02 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    long long input, count , degree = 3;
+    cin >> input;
+    count = input * input * input;
+    cout << count << endl << degree;
+    return 0;
+}
