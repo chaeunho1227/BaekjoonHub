5e5bb1640ee4a44785e415f7004dabff6e072937
chaeunho <97829504+chaeunho1227@users.noreply.github.com>
2025-04-07 19:02:08 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int input, count , degree = 1;
+    cin >> input;
+    count = input;
+    cout << count << endl << degree;
+    return 0;
+}
